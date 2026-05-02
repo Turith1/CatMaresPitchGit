5e6c14b4c04c1fff2ec52ffc63f1867de8db79f0
Turith1 <162712980+Turith1@users.noreply.github.com>
2026-05-02 02:08:49 -0300

    enemy fleeing

--- Assets/Scripts/ChasePlayer.cs
+++ Assets/Scripts/ChasePlayer.cs
@@ -10,17 +10,32 @@ public class ChasePlayer : StateMachineBehaviour
     private ActionGhosts enemyController;
     [SerializeField]
     private float _agentSpeed = 3.5f;
+    [SerializeField]
+    private PlayerCapture _playerCapture;
+    Transform bestSpot = null;
+    float maxDistance = -1;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         enemy = animator.GetComponentInParent<EnemyNavMesh>();
         enemyController = animator.GetComponentInParent<ActionGhosts>();
-
+        _playerCapture = enemyController.player.GetComponent<PlayerCapture>();
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         if (enemyController.player == null) return;
+
+
+        if (!_playerCapture.canCapture)
+        {
+            Debug.Log(!_playerCapture.canCapture);
+            enemy.m_agent.SetDestination(enemyController.player.position);
+        }
+        else if (_playerCapture.canCapture && !enemy.m_agent.pathPending)
+        {
+            FleeToBestSpot();
+        }
 
         if (!enemyController.IsPlayerInRange())
         {
@@ -28,11 +43,27 @@ public class ChasePlayer : StateMachineBehaviour
             animator.SetBool("Chase", false);
             return;
         }
+    }
 
-        enemy.m_agent.SetDestination(enemyController.player.position);
-        if (!enemyController._ronda.m_agent.pathPending && enemyController._ronda.m_agent.remainingDistance < 0.5f)
+    void FleeToBestSpot()
+    {
+
+        foreach (Transform spot in enemy._rotaEnemy)
         {
-            enemy.Ronda();
+            // Calcula a distância entre este ponto e o JOGADOR
+            float distFromPlayer = Vector3.Distance(spot.transform.position, enemyController.player.position);
+
+            // Queremos o ponto que esteja MAIS LONGE do jogador
+            if (distFromPlayer > maxDistance)
+            {
+                maxDistance = distFromPlayer;
+                bestSpot = spot;
+            }
+        }
+
+        if (bestSpot != null)
+        {
+            enemy.m_agent.SetDestination(bestSpot.transform.position);
         }
     }
 }
